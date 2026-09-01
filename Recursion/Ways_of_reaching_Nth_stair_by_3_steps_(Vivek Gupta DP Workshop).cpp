8babe48483687e1efac35cccf9a62fae70f54f03
SanjidaAhmed <60965597+SanjidaAhmed@users.noreply.github.com>
2026-09-02 05:02:32 +0600

    Implement recursive solution for stair climbing problem
    By 1, 2 or 3 steps in a turn, in how many ways one can reaches in Nth stair.
    T test cases, N stairs.
    Input:
    7
    4
    7
    3
    2
    1
    5
    12
    Output:
    4
    24
    2
    1
    1
    7
    504

--- Recursion/Ways_of_reaching_Nth_stair_by_3_steps_(Vivek Gupta DP Workshop).cpp
+++ Recursion/Ways_of_reaching_Nth_stair_by_3_steps_(Vivek Gupta DP Workshop).cpp
@@ -1,26 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n;
-int solve(int level)
+int rec(int level) // return -> number of way to N if we are at stair level.
 {
+   //level -> stair i am at
+
+   // pruning
    if(level > n) return 0;
+
+   // base case
    if(level == n) return 1;
+
    int ans = 0;
+   //loop over choice
    for(int step = 1; step <=3; step++){
       if(level + step <= n){
-         int ways = solve(level+step);
+         // check -> is a valid choice 
+
+         int ways = rec(level+step);//move
          ans += ways;
       }
    }
-   return ans;
+   return ans; // for this level
+}
+
+void solve(){
+   cin >> n;
+   cout << rec(1) << endl;
 }
 int main()
 {  
    int i, t;
    cin >> t;
    for(i=1; i<=t; i++){
-      cin >> n;
-      cout << solve(1) << endl;
+      solve();
    }
    return 0;
 }
